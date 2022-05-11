64f1967cc05811d632b2b640701e9867085ff3ed
hatz <hatz@localhost.localdomain>
2022-05-12 01:03:29 +0530

    asddds

--- pages/buy/[id]/index.js
+++ pages/buy/[id]/index.js
@@ -3,65 +3,98 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Link from "next/link";
 import Negoard from "../../../components/negocard"
+import { useEffect, useState } from "react"
 
 export default function ListProduct({ list, negolist }) {
-  const product = list.list[0];
-  const nego = negolist.list;
-  console.log(nego)
+  const [product, setproduct] = useState([])
+  const [nego, setnego] = useState([])
+  useEffect(() => {
+    (async () => {
+      const id = location.pathname.split("/");
+      const list = await fetch(`/api/list/${id[2]}`).then((res) =>
+        res.json()
+      );
+      if (list.success) {
+        console.log("li", list)
+        setproduct(list.list)
+      }
+      const negolist = await fetch(`/api/negotiateget?id=${id[2]}`).then((res) =>
+        res.json()
+      );
+      if (negolist.success) {
+        setnego(negolist.list)
+      }
+    })()
+
+  }, [])
+
+
+
+
+  // const product = list.list[0];
+  // const nego = negolist.list;
+  console.log("ne", nego)
+  console.log("pro", product)
   return (
     <>
-      <Grid container className="buy-product">
-        <Grid item className="buy-body">
-          <Paper elevation={0} className="buy-paper">
-            <Images
-              alt="cow"
-              className="buy-img"
-              src={product.link}
-              width={600}
-              height={600}
-            />
-          </Paper>
-        </Grid>
-        <Grid className="buy-body">
-          <Paper elevation={0} className="buy-paper">
-            <h1>{product.name}</h1>
-            <p>{product.detail}</p>
-            {product.dlist.map((a) => (
-              <ol key={a}>
-                <li>{a}</li>
-              </ol>
-            ))}
-            <h2>price:{product.price}</h2>
-            <Link className="buy-btn" href={`/buy/${product._id}/negotiate?id=${product._id}`}>
-              <button>buy</button>
-            </Link>
-          </Paper>
-        </Grid>
-      </Grid>
-      <div className="negolist">
-        <h2>negotiated list</h2>
-        <div className="nego-li">
-          {
-            nego.map(a => (
-              <Negoard key={a._id} {...a} />
-            )
-            )
-          }
-        </div>
-      </div>
+      {
+        nego.length && product.length ?
+          <>
+            <Grid container className="buy-product">
+              <Grid item className="buy-body">
+                <Paper elevation={0} className="buy-paper">
+                  <Images
+                    alt="cow"
+                    className="buy-img"
+                    src={product[0].link}
+                    width={600}
+                    height={600}
+                  />
+                </Paper>
+              </Grid>
+              <Grid className="buy-body">
+                <Paper elevation={0} className="buy-paper">
+                  <h1>{product[0].name}</h1>
+                  <p>{product[0].detail}</p>
+                  {product[0].dlist.map((a) => (
+                    <ol key={a}>
+                      <li>{a}</li>
+                    </ol>
+                  ))}
+                  <h2>price:{product[0].price}</h2>
+                  <Link className="buy-btn" href={`/buy/${product[0]._id}/negotiate?id=${product[0]._id}`}>
+                    <button>buy</button>
+                  </Link>
+                </Paper>
+              </Grid>
+            </Grid>
+            <div className="negolist">
+              <h2>negotiated list</h2>
+              <div className="nego-li">
+                {
+                  nego.map(a => (
+                    <Negoard key={a._id} {...a} />
+                  )
+                  )
+                }
+              </div>
+            </div>
+          </> : ""
+      }
+
     </>
   );
 }
 
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-  const list = await fetch(`/api/list/${id}`).then((res) =>
-    res.json()
-  );
-  const negolist = await fetch(`/api/negotiateget?id=${id}`).then((res) =>
-    res.json()
-  );
-  return {
-    props: { list, negolist },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const { id } = context.params;
+//   const list = await fetch(`/api/list/${id}`).then((res) =>
+//     res.json()
+//   );
+//   const negolist = await fetch(`/api/negotiateget?id=${id}`).then((res) =>
+//     res.json()
+//   );
+//   return {
+//     props: { list, negolist },
+//   };
+// }
