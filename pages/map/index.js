2e81528e0d8579ee2f1e511b128dea9843c5ce6c
hatz <hatz@localhost.localdomain>
2022-05-12 01:37:17 +0530

    asdddsa

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,15 +1,24 @@
-export default function Map() {
+export default function Mapp() {
   return (
     <>
       <div className="map-container">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62520.73660553907!2d78.16436572216459!3d11.65566250009118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf2958b116da1%3A0x16bd3d7888a1e012!2sWeekly%20Market!5e0!3m2!1sen!2sin!4v1652274618093!5m2!1sen!2sin"
-          width="600"
-          height="450"
-          style={{ border: 0 }}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <h1>Milk Society in salem</h1>
+        <div className="map-body">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31260.054720206535!2d78.122303915625!3d11.658448400000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf03340217865%3A0x1bfdfdd62ce52f0!2sSalem%20Milk%20Production%20Society!5e0!3m2!1sen!2sin!4v1652298273491!5m2!1sen!2sin"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade">
+          </iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31260.054720206535!2d78.122303915625!3d11.658448400000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf1b52d063b0b%3A0x9c54e7bee52adf14!2sKumarasamipatti%20Milk%20Producers%20Cooperative%20Society!5e0!3m2!1sen!2sin!4v1652299181449!5m2!1sen!2sin" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31259.28823183787!2d78.05797611562498!3d11.665255199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babfa95aaaaaaab%3A0x7b349e84e9c6600d!2sSalem%20District%20Cooperative%20Milk%20Producers%20Union%20Ltd.%2C!5e0!3m2!1sen!2sin!4v1652299228539!5m2!1sen!2sin" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31258.842975614854!2d78.14929111562502!3d11.669207500000008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf194ff9c0ae9%3A0x1ebe89a33010af1c!2sMilk%20Society!5e0!3m2!1sen!2sin!4v1652299252507!5m2!1sen!2sin" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31258.84567984363!2d78.051977515625!3d11.669183500000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babfaec63de9985%3A0xb8e5ebcc3fb2edac!2zU2FsZW0gRGlzdHJpY3QgRGFpcnksIOCumuCvh-CusuCuruCvjSDgrqrgrr7grrLgr40g4K6q4K6j4K-N4K6j4K-I!5e0!3m2!1sen!2sin!4v1652299295086!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31258.029396118167!2d78.20299361562503!3d11.676425800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf3c41257a141%3A0xb03f3db4574ac9f0!2sMilk%20society!5e0!3m2!1sen!2sin!4v1652299324964!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31258.177884358032!2d78.08522111562502!3d11.67510869999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf1529444ac69%3A0x1e25c205d6eaf1fb!2sDoodla%20dairy%20suramangalam%20salem!5e0!3m2!1sen!2sin!4v1652299446535!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31261.857156405662!2d78.112966815625!3d11.642426400000017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babefd7e30732f9%3A0x5e587069fa983ef8!2sSri%20Angalamman%20Milk%20Agencies!5e0!3m2!1sen!2sin!4v1652299468056!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31258.80039455613!2d77.95668171562502!3d11.669585400000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babfdd4d5d8bd61%3A0xb386d4ed662f3536!2sMilk%20society%20Anaimedu!5e0!3m2!1sen!2sin!4v1652299508027!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31260.185389531583!2d78.12358621562502!3d11.65728760000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf15212d0ebcd%3A0xaef4a7d990b31991!2sAavin%20milk%20parlour!5e0!3m2!1sen!2sin!4v1652299560783!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
       </div>
     </>
   );
